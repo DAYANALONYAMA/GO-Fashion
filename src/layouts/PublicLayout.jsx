ced87a3010c9df47b6c771bb218877185bfac4d1
rkota <rosairekota@gmail.com>
2023-04-24 16:30:03 +0100

    fix: resolve store issue

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,7 +1,7 @@
 
 import { Outlet } from "react-router-dom";
 import Footer from "../componemts/Footer/Footer";
-import Navbar from ".:/componemts/Navbar/Navbar";
+import Navbar from "../componemts/Navbar/Navbar";
 export const Layout = () => {
     return (
       <div className="app">
